d48128382a5ae1609e53fd29247175e601f3260e
iRa <ira.susanto@racerocks3d.com>
2025-04-14 13:50:40 -0700

    Setup email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :enable_starttls_auto => true,
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV["MAIL_USERNAME"],
+  :password => ENV["MAIL_PASSWORD"],
+  :openssl_verify_mode => 'none'
+}
